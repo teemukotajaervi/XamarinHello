bd6510a91ea99d158a327beb98152558c3b1e493
Teemu Kotajärvi <teemu.kotajarvi@stud.pointcollege.fi>
2017-12-03 13:37:47 +0200

    Päivitetty käyttöliittymän tekstia.

---
+++ XamarinHello/XamarinHello/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinHello
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                {
+                    new Label
+                    {
+                        HorizontalTextAlignment = TextAlignment.Center,
+                        Text = "Heissan Xamarin!!",
+                        TextColor = Color.Yellow
+                    }
+                }
+             }
+          };
+       }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
